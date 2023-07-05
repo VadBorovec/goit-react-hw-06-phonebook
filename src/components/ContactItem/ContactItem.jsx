e426b9910dce38e9308767915fa3bea9ea00cde8
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-05 21:58:50 +0300

    Update ContactItem.jsx

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,16 +3,16 @@ import { Name, Number } from './ContactItem.styled';
 import { useDispatch } from 'react-redux';
 import { remove } from 'redux/contactsSlice';
 
-export const ContactItem = ({ id, name, number }) => (
-    const dispatch = useDispatch();
-    const handleDelete = (id) => dispatch(remove(id));;
+export const ContactItem = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+  const handleDelete = id => dispatch(remove(id));
 
-return (
-      <>
-    <Name>{name}</Name>
-    <Number>{number}</Number>
-    {/* <Button onClick={() => console.log(id)}>Delete</Button> */}
-    <Button onClick={handleDelete}>Delete</Button>
-  </>
-)
-);
+  return (
+    <>
+      <Name>{name}</Name>
+      <Number>{number}</Number>
+      {/* <Button onClick={() => console.log(id)}>Delete</Button> */}
+      <Button onClick={handleDelete}>Delete</Button>
+    </>
+  );
+};
